2aacf29a68eaf4c15a2aa0997aed5951367806c6
Steven Arycena Fatich <arycenas@gmail.com>
2025-07-15 11:28:42 +0700

    first commit

---
+++ src/test/java/com/experiment/caching/CachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experiment.caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
